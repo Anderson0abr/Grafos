edd17fe636958f79f2a9ac36be9349ba64178cf5
Anderson0abr <anderson0abr@gmail.com>
2017-05-23 19:36:43 -0300

    Montadas matrizes de custo para os 3 tipos de arquivos

--- src/algoritmos/VizinhoMaisProximo.java
+++ src/algoritmos/VizinhoMaisProximo.java
@@ -57,10 +57,8 @@ public class VizinhoMaisProximo {
 			}
 		}
 		int lineIndex = 0;
-		for(String line : fileLines){
-			fileLines.set(lineIndex, fileLines.get(lineIndex).trim().replaceAll(" +", " "));
-			lineIndex++;
-		}
+		for(String line : fileLines)
+			fileLines.set(lineIndex, fileLines.get(lineIndex++).trim().replaceAll(" +", " "));
 		return fileLines;
 	}
 
@@ -78,22 +76,30 @@ public class VizinhoMaisProximo {
 		return type;
 	}
 
+	private static Double DistanciaEntrePontos(Double[] coord1, Double[] coord2) {
+		return Math.sqrt(Math.pow(coord2[0]-coord1[0], 2.0) + Math.pow(coord2[1]-coord1[1], 2.0));
+	}
+
 	private static void type1(List<String> fileLines, int n) {
 		List<Double[]> custos = new ArrayList<Double[]>();
-				
+		Double[] line = new Double[n];
+		
 		for(int i = 1; i < n; i++){
 			int j = 0;
-			Double[] line = new Double[n-i+1];
-			
+			while(i > j + 1)
+				line[j] = custos.get(j++)[i-1];
+			line[j++] = 0.0;
 			for(String value: fileLines.get(i).split(" ")){
-				line[j] = Double.parseDouble(value);
-				j++;
+				line[j++] = Double.parseDouble(value);
 			}
-			line[j] = 0.0;
 			custos.add(line.clone());
 		}
-		custos.add(new Double[]{0.0});
-		//teste
+		for(int j = 0; j < n-1; j++)
+			line[j] = custos.get(j)[n-1];
+		line[n-1] = 0.0;
+		custos.add(line.clone());
+		
+		// TESTE - APAGAR DEPOIS
 		for(int y = 0; y < n; y++){
 			for(Double num: custos.get(y))
 				System.out.print(num.toString() + " ");
@@ -103,8 +109,34 @@ public class VizinhoMaisProximo {
 
 	private static void type2(List<String> fileLines, int n) {
 		List<Double[]> custos = new ArrayList<Double[]>();
-		//Pendente
+		List<Double[]> coordenadas = new ArrayList<Double[]>();
 		
+		for(int i = 1; i <= n; i++){
+			Double[] line = new Double[2];
+			int j = 0;
+			for(String value: fileLines.get(i).split(" ")){
+				line[j++] = Double.parseDouble(value);
+			}
+			coordenadas.add(line.clone());
+		}
+		
+		for(int i = 0; i < n; i++){
+			int j = 0;
+			Double[] line = new Double[n];
+			while(i > j + 1)
+				line[j] = custos.get(j++)[i-1];
+			line[j++] = 0.0;
+			while(j < n)
+				line[j] = DistanciaEntrePontos(coordenadas.get(i), coordenadas.get(j++));
+			custos.add(line.clone());
+		}
+		
+		// TESTE - APAGAR DEPOIS
+		for(int y = 0; y < n; y++){
+			for(Double num: custos.get(y))
+				System.out.print(num.toString() + " ");
+			System.out.println();
+		}
 	}
 
 	private static void type3(List<String> fileLines, int n) {
@@ -113,20 +145,17 @@ public class VizinhoMaisProximo {
 		
 		for(int i = 1; i <= n; i++){
 			int j = 0;
-			for(String value: fileLines.get(i).split(" ")){
-				line[j] = Double.parseDouble(value);
-				j++;
-			}
+			for(String value: fileLines.get(i).split(" "))
+				line[j++] = Double.parseDouble(value);
 			custos.add(line.clone());
 		}
 		
-		//teste
+		// TESTE - APAGAR DEPOIS
 		for(int y = 0; y < n; y++){
 			for(Double num: custos.get(y))
 				System.out.print(num.toString() + " ");
 			System.out.println();
 		}
-		
 	}
 
 }
